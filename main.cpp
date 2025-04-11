b274ea68185500abb5ab38dfd9634a18726d2a7a
Your Name <naveen.ottilingam@gmail.com>
2025-04-11 18:19:40 +0530

    finish sprite loading, screens and trie and word implementaion

--- main.cpp
+++ main.cpp
@@ -1,18 +1,74 @@
 #include "raylib.h"
 #include <iostream>
 #include "tries.h"
+#include "words.h"
 #include <string>
+#include <ctime>
+#include <cstdlib>
 
+using namespace std;
+struct Asteroid{
+    string word;
+    Vector2 pos;
+    float speed;
+    bool isTargeted = false;
+    Asteroid(string w, Vector p, float sp): word(w), pos(p), speed(sp) {}
+};
+typedef enum GameScreen {MENU = 0, GAMEPLAY, ENDING} GameScreen;
 int main() {
+    Trie trie;
     const int HEIGHT = 720;
     const int WIDTH = 480;
     InitWindow(WIDTH, HEIGHT, "Typing Game");
+    const Texture2D shooter = LoadTexture("shooter.png");
+    string currentInput = "";
+    float rotation = 0;
+    int score = 0;
+    int index = 0;
+    int wave;
+    int asteroidCount = 0;
+    int generateTimer = 0;
+    int currentTime = 0;
+    vector<Asteroid> asteroids;
     SetTargetFPS(60);
+    GameScreen currentScreen = MENU;
+    srand(time());
     while(!WindowShouldClose()){
+        switch(currentScreen){
+            case MENU:
+            if(IsKeyPressed(KEY_ENTER)){
+                currentScreen = GAMEPLAY;
+            }
+            break;
+            case GAMEPLAY:
+
+            break;
+            case ENDING:
+
+            break;
+            default: break;
+        }
+        index = rand() % 100;
+        trie.insert(words[index]);
+        asteroids.push_back(new Asteroid(words[index], {GetRandomValue(30, 400), 100}, GetRandomValue(3, 10)));
         BeginDrawing();
-        DrawText("Game", 100, 100, 100, WHITE);
+        switch(currentScreen){
+            case MENU:
+            DrawRectangle(0, 0, WIDTH, HEIGHT, GRAY);
+            DrawText( "Press Enter To Play", 25, 50, 20, BLACK);
+            break;
+            case GAMEPLAY:
+            DrawRectangle(0, 0, WIDTH, HEIGHT, RAYWHITE);
+
+            DrawTextureEx(shooter, {175, 500}, rotation, 0.5f, WHITE);
+            break;
+            case ENDING:
+            break;         
+            default: break;
+        }
         EndDrawing();
     }
+    UnloadTexture(shooter);
     CloseWindow();
     return 0;
     
---
+++ words.h
@@ -0,0 +1,21 @@
+#ifndef WORDS_H
+#define WORDS_H
+#include <vector>
+#include <string>
+using namespace std;
+const vector<string> words = {
+    "rocket", "asteroid", "galaxy", "comet", "planet", "orbit", "gravity", "meteor", "satellite",
+    "universe", "nebula", "eclipse", "lunar", "solar", "cosmos", "blackhole", "lightyear", "quasar",
+    "supernova", "telescope", "spaceship", "module", "launch", "astronaut", "probe", "module",
+    "milkyway", "nasa", "alien", "extraterrestrial", "radiation", "spacetime", "wormhole", "star",
+    "trajectory", "rover", "capsule", "thruster", "ion", "fuel", "oxygen", "spacesuit", "apollo",
+    "voyager", "mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune", "pluto",
+    "titan", "europa", "deimos", "phobos", "asterism", "atmosphere", "axis", "celestial", "constellation",
+    "core", "darkmatter", "dust", "flare", "gamma", "helium", "hubble", "impact", "infrared", "juno",
+    "launchpad", "magnitude", "mission", "moondust", "observatory", "orbiters", "pulsar", "quantum",
+    "reentry", "satcom", "skywalk", "spaceshuttle", "spectrum", "stardust", "starlight", "station",
+    "stratosphere", "sunspot", "telemetry", "thrusters", "titanium", "vacuum", "velocity", "zenith",
+    "zenon", "turbulence", "navigation", "gravitywell", "liftoff"
+};
+
+#endif
